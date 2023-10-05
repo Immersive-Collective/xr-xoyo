07745b1686921ad88361f31cd14b38528729cf87
Koshi <koshicontact@gmail.com>
2023-10-05 18:43:43 +0200

    two more textures added

--- app/src/index.js
+++ app/src/index.js
@@ -648,7 +648,11 @@ let sceneConfig = [
     {texture: "StageSmall1_512", videoTextureSrc: "apple_5"},
     {texture: "StageSmall2_512", videoTextureSrc: "apple_1"},
     {texture: "StageSmall3_512", videoTextureSrc: "apple_6"},
+    {texture: "RearScreen", videoTextureSrc: "apple_1"},
+    {texture: "RearCeiling_1024", videoTextureSrc: "apple_2"},
     {texture: "MegaScreen", videoTextureSrc: "apple_6"}
+
+
 
 ];
 
